3a8949ca30275016c2ceb90ad8c8466984686add
Kenneth Muturi <muturiken@gmail.com>
2025-10-02 22:42:28 +0300

    testing form builder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, VStack } from "@chakra-ui/react";
 import React from "react";
 import GenericPage from "@/components/Generic/Page";
 
 const page = () => {
   return (
-    <GenericPage title={"Welcome to the Forms"}>
-      <Box>New forms reports and such will come here .......... (report)</Box>
-    </GenericPage>
+    <Flex
+      as="main"
+      w="full"
+      h="full"
+      minH="100vh"
+      alignItems="start"
+      justifyContent="start"
+      flexDirection="column"
+    >
+      <VStack w="full" h="full" borderRadius="md" alignItems="left">
+        <GenericPage title={"Welcome to the Forms"}>
+          <Box>
+            New forms reports and such will come here .......... (report)
+          </Box>
+        </GenericPage>
+      </VStack>
+    </Flex>
   );
 };
 
